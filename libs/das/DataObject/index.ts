842ec643a37c1d1c3be1e84b5bbe11abd7099a83
wangjiegj <wangjiegj@inspur.com>
2018-05-02 19:23:48 +0800

    删除das相关lib。

--- libs/das/DataObject/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/Column';
-export * from './src/DataObjectType';
-export * from './src/DataSourceConnectionType';
-export * from './src/DataType';
-export * from './src/GSPDataTable';
--- libs/dasSample/user/userSample.ts
+++
@@ -1,6 +0,0 @@
-export const userSample=
-    {
-        ID:undefined,
-        Code : "XMXMINFO_XMNM",
-        Name : "项目内码",
-    }
